12ed788d52ad42ca060ac9cc5cec018f255837dd
Philip Aduteye <philedu30@gmail.com>
2024-02-06 12:24:22 +0100

    branch-philip

---
+++ index.js
@@ -0,0 +1,16 @@
+const button = document.getElementById("proceedButton");
+const checkbox = document.getElementById("myCheckbox");
+
+function illumina() {
+    if (checkbox.checked) {
+        button.disabled = false;
+        button.classList.add("buttonChecked"); 
+    } else {
+        button.disabled = true;
+        button.classList.remove("buttonChecked");
+    }
+}
+
+document.getElementById("proceedButton").addEventListener("click", function() {
+    window.location.href = './benchmark.html';
+});
