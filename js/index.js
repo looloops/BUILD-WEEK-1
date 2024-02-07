7df368096c418d8bfc79198f4fe276acb799fd87
Philip Aduteye <philedu30@gmail.com>
2024-02-07 12:51:22 +0100

    button js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 const button = document.getElementById("proceedButton");
 const checkbox = document.getElementById("myCheckbox");
-/* 
+
 function brightColour() {
     if (checkbox.checked) {
         button.disabled = false;
-        button.classList.add("buttonChecked"); 
+        button.classList.add("btn");  
     } else {
         button.disabled = true;
-        button.classList.remove("buttonChecked");
-    }
-} */
-
+        button.classList.remove("btn");
+    } 
+}
 
 document.getElementById("proceedButton").addEventListener("click", function() {
     window.location.href = './benchmark.html';
